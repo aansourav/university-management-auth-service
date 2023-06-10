f80129dd663ac4c93d2e860707f789afba5742ae
aansourav <aansourav@gmail.com>
2023-06-10 16:17:51 +0600

    setup eslint husky

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Connected Successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Connected Successfully')
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
-    });
+      console.log(`App listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Failed to Connect", error);
+    console.log('Failed to Connect', error)
   }
 }
-bootstrap();
+bootstrap()
